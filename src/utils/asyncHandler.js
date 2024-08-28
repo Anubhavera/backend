bcf361e0f6584b7c4dc4ee2b3bbc27c0b949158b
ANUBHAV HOODA <102412479+Anubhavera@users.noreply.github.com>
2024-08-28 17:14:40 +0530

    util code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = () => {
+  (req,res,next) => {
+    Promise.resolve(requestHandler(req,res,next))
+    .catch((err) => next(err))
+  }
+}
+
+
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = (fn) => async(req,res,next) => {
+//   try{
+
+//   }catch(error){
+//     res.status(err.code || 500 ).json({
+
+//     })
+//   }
+// }
