202728e1dc2267a779bbe27839719857aac176c0
ANUBHAV HOODA <102412479+Anubhavera@users.noreply.github.com>
2024-08-28 21:28:43 +0530

    adding video and user models for mongo db

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,1 +1,48 @@
+ import mongoose from "mongoose";
+ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+ const videoSchema = new Schema(
+  {
+    videoFile :{
+      type : String, //cloudinary url 
+      required : true, 
+    },
+    videoFile :{
+      type : String, //cloudinary url 
+      required : true, 
+    }, 
+    Title :{
+      type : String, //cloudinary url 
+      required : true, 
+    }, 
+    Description :{
+      type : String, //cloudinary url 
+      required : true, 
+    }
+  ,
+   duration : {
+    type : String, //cloudinary url 
+    required : true, 
+  },
+  views : {
+    type : Number,
+    default:0
+  },
+  isPublished :{
+    type : Boolean, 
+    default:true 
+  },
+  owner :{
+    type : Schema.Types.ObjectId,
+    ref:"User"
+  },
+  
+  },{
+    timestamps: true
+  }
+ )
  
+
+ videoSchema.plugin(mongooseAggregatePaginate)
+ export const Video = mongoose.model("Video", videoSchema)
